b34161c2e9721d6f32d969cd9db9aa2109998733
bLeDy <deejaynof@gmail.com>
2021-02-06 15:20:08 +0100

    tmux: use `~/` location as it doesn't like the `$XDG_CONFIG_HOME` one

--- get_dotfiles.bash
+++ get_dotfiles.bash
@@ -16,12 +16,13 @@ cp ~/.config/i3blocks/config .config/i3blocks/
 cp ~/.config/mpv/mpv.conf .config/mpv/
 cp ~/.config/pulse/daemon.conf .config/pulse/
 cp ~/.config/sublime-text-3/Packages/User/{"Default (Linux).sublime-keymap","Package Control.sublime-settings",Preferences.sublime-settings} .config/sublime-text-3/Packages/User/
-cp ~/.config/tmux/tmux.conf .config/tmux/
+# cp ~/.config/tmux/tmux.conf .config/tmux/
 cp ~/.config/vim/vimrc .config/vim/
 cp ~/.config/zsh/.{p10k.zsh,zshrc} .config/zsh/
 # cp ~/.local/bin/{countdown,extramaus,fix-volume,mute-spotify-ads} .local/bin/
 cp ~/.profile .
 cp ~/.TilingAssistantExtension.layouts.json .
+cp ~/.tmux.conf .
 cp ~/.ssh/config .ssh/
 
 sudo cp etc/asound.conf /etc/
