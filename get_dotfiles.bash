1e82367219413f714ea2f21cf2544fba916e406f
bLeDy <deejaynof@gmail.com>
2021-02-06 14:45:38 +0100

    Move `extensions-sync.json` and add `.TilingAssistantExtension.layouts.json`

--- get_dotfiles.bash
+++ get_dotfiles.bash
@@ -1,5 +1,5 @@
 #!/bin/bash
-mkdir -p .config/{alacritty,Code/User,dash-to-panel,flameshot,git,htop,i3,i3blocks,mpv,pulse,sublime-text-3/Packages/User,tmux,vim,zsh}
+mkdir -p .config/{alacritty,Code/User,dash-to-panel,flameshot,git,gnome-extensions-sync,htop,i3,i3blocks,mpv,pulse,sublime-text-3/Packages/User,tmux,vim,zsh}
 mkdir -p .local/bin
 mkdir -p .ssh
 mkdir -p etc/{X11,default}
@@ -7,9 +7,9 @@ mkdir -p etc/{X11,default}
 cp ~/.config/alacritty/alacritty.yml .config/alacritty/
 cp ~/.config/Code/User/{keybindings.json,settings.json} .config/Code/User/
 cp ~/.config/dash-to-panel/dash-to-panel-settings .config/dash-to-panel/
-cp ~/.config/extensions-sync.json .config
 cp ~/.config/flameshot/flameshot.conf .config/flameshot/
 cp ~/.config/git/config .config/git/
+cp ~/.config/gnome-extensions-sync/extensions.json .config/gnome-extensions-sync
 cp ~/.config/htop/htoprc .config/htop/
 cp ~/.config/i3/config .config/i3/
 cp ~/.config/i3blocks/config .config/i3blocks/
@@ -25,3 +25,4 @@ sudo cp etc/default/grub /etc/default/
 # cp ~/.local/bin/{countdown,extramaus,fix-volume,mute-spotify-ads} .local/bin/
 cp .profile ~/
 cp .ssh/config ~/.ssh/
+cp ~/.TilingAssistantExtension.layouts.json .
--- set_dotfiles.bash
+++ set_dotfiles.bash
@@ -1,12 +1,14 @@
 #!/bin/bash
 mkdir -p ~/.cache/zsh
-mkdir -p ~/.config/{alacritty,Code/User,dash-to-panel,flameshot,git,htop,i3,i3blocks,mpv,pulse,sublime-text-3/Packages/User,tmux,vim,zsh}
+mkdir -p ~/.config/{alacritty,Code/User,dash-to-panel,flameshot,git,gnome-extensions-sync,htop,i3,i3blocks,mpv,pulse,sublime-text-3/Packages/User,tmux,vim,zsh}
 
+cp .config/aliasrc ~/.config
 cp .config/alacritty/alacritty.yml ~/.config/alacritty/
 cp .config/Code/User/{keybindings.json,settings.json} ~/.config/Code/User/
 cp .config/dash-to-panel/dash-to-panel-settings ~/.config/dash-to-panel/
 cp .config/flameshot/flameshot.conf ~/.config/flameshot/
 cp .config/git/config ~/.config/git/
+cp .config/gnome-extensions-sync/extensions.json ~/.config/gnome-extensions-sync/
 cp .config/htop/htoprc ~/.config/htop/
 cp .config/i3/config ~/.config/i3/
 cp .config/i3blocks/config ~/.config/i3blocks/
@@ -21,3 +23,4 @@ sudo cp etc/default/grub /etc/default/
 # cp .local/bin/{countdown,extramaus,fix-volume,mute-spotify-ads} ~/.local/bin/
 cp .profile ~/
 cp .ssh/config ~/.ssh/
+cp .TilingAssistantExtension.layouts.json ~/
