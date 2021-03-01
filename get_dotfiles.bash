22ab2a9405ef2b3530c7e4580248fe7a63837b64
Iago Alonso <deejaynof@gmail.com>
2021-03-01 19:20:11 +0100

    scripts: Use variables for destination dirs

--- get_dotfiles.bash
+++ get_dotfiles.bash
@@ -1,30 +1,37 @@
 #!/bin/bash
-mkdir -p .config/{alacritty,Code/User,dash-to-panel,flameshot,git,gnome-extensions-sync,htop,i3,i3blocks,mpv,pulse,sublime-text-3/Packages/User,tmux,vim,zsh}
-mkdir -p .local/bin
-mkdir -p .ssh
-mkdir -p etc/{X11,default}
 
-cp ~/.config/alacritty/alacritty.yml .config/alacritty/
-cp ~/.config/Code/User/{keybindings.json,settings.json} .config/Code/User/
-cp ~/.config/dash-to-panel/dash-to-panel-settings .config/dash-to-panel/
-cp ~/.config/flameshot/flameshot.conf .config/flameshot/
-cp ~/.config/git/config .config/git/
-cp ~/.config/gnome-extensions-sync/extensions.json .config/gnome-extensions-sync
-cp ~/.config/htop/htoprc .config/htop/
-cp ~/.config/i3/config .config/i3/
-cp ~/.config/i3blocks/config .config/i3blocks/
-cp ~/.config/mpv/mpv.conf .config/mpv/
-cp ~/.config/pulse/daemon.conf .config/pulse/
-cp ~/.config/sublime-text-3/Packages/User/{"Default (Linux).sublime-keymap","Package Control.sublime-settings",Preferences.sublime-settings} .config/sublime-text-3/Packages/User/
-# cp ~/.config/tmux/tmux.conf .config/tmux/
-cp ~/.config/vim/vimrc .config/vim/
-cp ~/.config/zsh/.{p10k.zsh,zshrc} .config/zsh/
-# cp ~/.local/bin/{countdown,extramaus,fix-volume,mute-spotify-ads} .local/bin/
-cp ~/.profile .
-cp ~/.TilingAssistantExtension.layouts.json .
-cp ~/.tmux.conf .
-cp ~/.ssh/config .ssh/
+DOTFILES_HOME="$HOME/workspace/dotfiles"
+DOTFILES_CONFIG_HOME="$DOTFILES_HOME/.config"
+DOTFILES_LOCAL_BIN="$DOTFILES_HOME/.local/bin"
+DOTFILES_ETC="$DOTFILES_HOME/etc"
+DOTFILES_USR_SHARE="$DOTFILES_HOME/usr/share"
 
-sudo cp /etc/asound.conf etc/
-sudo cp /etc/X11/xorg.conf etc/X11/
-sudo cp /etc/default/grub etc/default/
+# $HOME
+mkdir -p $DOTFILES_HOME/.ssh
+cp ~/.asoundrc $DOTFILES_HOME
+cp ~/.pdbrc $DOTFILES_HOME
+cp ~/.profile $DOTFILES_HOME
+cp ~/.TilingAssistantExtension.layouts.json $DOTFILES_HOME
+
+# $XDG_CONFIG_HOME
+mkdir -p $DOTFILES_CONFIG_HOME/{alacritty,Code/User,dash-to-panel,flameshot,git,gnome-extensions-sync,htop,i3,i3blocks,mpv,pulse,sublime-text-3/Packages/User,tmux,vim,zsh}
+cp ~/.config/alacritty/alacritty.yml $DOTFILES_CONFIG_HOME/alacritty/
+cp ~/.config/Code/User/{keybindings.json,settings.json} $DOTFILES_CONFIG_HOME/Code/User/
+cp ~/.config/dash-to-panel/dash-to-panel-settings $DOTFILES_CONFIG_HOME/dash-to-panel/
+cp ~/.config/flameshot/flameshot.conf $DOTFILES_CONFIG_HOME/flameshot/
+cp ~/.config/git/config $DOTFILES_CONFIG_HOME/git/
+cp ~/.config/gnome-extensions-sync/extensions.json $DOTFILES_CONFIG_HOME/gnome-extensions-sync
+cp ~/.config/htop/htoprc $DOTFILES_CONFIG_HOME/htop/
+cp ~/.config/i3/config $DOTFILES_CONFIG_HOME/i3/
+cp ~/.config/i3blocks/config $DOTFILES_CONFIG_HOME/i3blocks/
+cp ~/.config/mpv/mpv.conf $DOTFILES_CONFIG_HOME/mpv/
+cp ~/.config/pulse/daemon.conf $DOTFILES_CONFIG_HOME/pulse/
+cp ~/.config/sublime-text-3/Packages/User/{"Default (Linux).sublime-keymap","Package Control.sublime-settings",Preferences.sublime-settings} $DOTFILES_CONFIG_HOME/sublime-text-3/Packages/User/
+cp ~/.config/tmux/tmux.conf $DOTFILES_CONFIG_HOME/tmux/
+cp ~/.config/vim/vimrc $DOTFILES_CONFIG_HOME/vim/
+cp ~/.config/zsh/.{p10k.zsh,zshrc} $DOTFILES_CONFIG_HOME/zsh/
+
+# $HOME/.local/bin
+mkdir -p $DOTFILES_LOCAL_BIN
+# cp ~/.local/bin/{countdown,extramaus,fix-volume,mute-spotify-ads} $DOTFILES_LOCAL_BIN
+cp ~/.local/bin/fix-screen-tearing $DOTFILES_LOCAL_BIN
--- set_dotfiles.bash
+++ set_dotfiles.bash
@@ -1,28 +1,36 @@
 #!/bin/bash
-mkdir -p ~/.cache/zsh
-mkdir -p ~/.config/{alacritty,Code/User,dash-to-panel,flameshot,git,gnome-extensions-sync,htop,i3,i3blocks,mpv,pulse,sublime-text-3/Packages/User,tmux,vim,zsh}
 
-cp .config/aliasrc ~/.config
-cp .config/alacritty/alacritty.yml ~/.config/alacritty/
-cp .config/Code/User/{keybindings.json,settings.json} ~/.config/Code/User/
-cp .config/dash-to-panel/dash-to-panel-settings ~/.config/dash-to-panel/
-cp .config/flameshot/flameshot.conf ~/.config/flameshot/
-cp .config/git/config ~/.config/git/
-cp .config/gnome-extensions-sync/extensions.json ~/.config/gnome-extensions-sync/
-cp .config/htop/htoprc ~/.config/htop/
-cp .config/i3/config ~/.config/i3/
-cp .config/i3blocks/config ~/.config/i3blocks/
-cp .config/mpv/mpv.conf ~/.config/mpv/
-cp .config/pulse/daemon.conf ~/.config/pulse/
-cp .config/sublime-text-3/Packages/User/{"Default (Linux).sublime-keymap","Package Control.sublime-settings",Preferences.sublime-settings} ~/.config/sublime-text-3/Packages/User/
-# cp .config/tmux/tmux.conf ~/.config/tmux/
+DOTFILES_HOME="$HOME/workspace/dotfiles"
+DOTFILES_CONFIG_HOME="$DOTFILES_HOME/.config"
+DOTFILES_LOCAL_BIN="$DOTFILES_HOME/.local/bin"
+DOTFILES_ETC="$DOTFILES_HOME/etc"
+DOTFILES_USR_SHARE="$DOTFILES_HOME/usr/share"
+
+# $HOME
+cp $DOTFILES_HOME/.asoundrc ~/
+cp $DOTFILES_HOME/.profile ~/
+cp $DOTFILES_HOME/.TilingAssistantExtension.layouts.json ~/
+cp $DOTFILES_HOME/.ssh/config ~/.ssh/
+
+# $XDG_CONFIG_HOME
+mkdir -p ~/.config/{alacritty,Code/User,dash-to-panel,flameshot,git,gnome-extensions-sync,htop,i3,i3blocks,mpv,pulse,sublime-text-3/Packages/User,tmux,vim,zsh}
+cp $DOTFILES_CONFIG_HOME/.config/aliasrc ~/.config
+cp $DOTFILES_CONFIG_HOME/.config/alacritty/alacritty.yml ~/.config/alacritty/
+cp $DOTFILES_CONFIG_HOME/.config/Code/User/{keybindings.json,settings.json} ~/.config/Code/User/
+cp $DOTFILES_CONFIG_HOME/.config/dash-to-panel/dash-to-panel-settings ~/.config/dash-to-panel/
+cp $DOTFILES_CONFIG_HOME/.config/flameshot/flameshot.conf ~/.config/flameshot/
+cp $DOTFILES_CONFIG_HOME/.config/git/config ~/.config/git/
+cp $DOTFILES_CONFIG_HOME/.config/gnome-extensions-sync/extensions.json ~/.config/gnome-extensions-sync/
+cp $DOTFILES_CONFIG_HOME/.config/htop/htoprc ~/.config/htop/
+cp $DOTFILES_CONFIG_HOME/.config/i3/config ~/.config/i3/
+cp $DOTFILES_CONFIG_HOME/.config/i3blocks/config ~/.config/i3blocks/
+cp $DOTFILES_CONFIG_HOME/.config/mpv/mpv.conf ~/.config/mpv/
+cp $DOTFILES_CONFIG_HOME/.config/pulse/daemon.conf ~/.config/pulse/
+cp $DOTFILES_CONFIG_HOME/.config/sublime-text-3/Packages/User/{"Default (Linux).sublime-keymap","Package Control.sublime-settings",Preferences.sublime-settings} ~/.config/sublime-text-3/Packages/User/
+cp .config/tmux/tmux.conf ~/.config/tmux/
 cp .config/vim/vimrc ~/.config/vim/
 cp .config/zsh/.{p10k.zsh,zshrc} ~/.config/zsh/
-# cp .local/bin/{countdown,extramaus,fix-volume,mute-spotify-ads} ~/.local/bin/
-cp .profile ~/
-cp .TilingAssistantExtension.layouts.json ~/
-cp .tmux.conf ~/
-cp .ssh/config ~/.ssh/
 
-sudo cp etc/asound.conf /etc/
-sudo cp etc/default/grub /etc/default/
+# $HOME/.local/bin
+# mkdir -p ~/.local/bin
+# cp .local/bin/{countdown,extramaus,fix-volume,mute-spotify-ads} ~/.local/bin/
