c865a6acaa3a98bb6056bcc4feb702d14e25bcb8
Iago Alonso <deejaynof@gmail.com>
2021-03-26 20:06:19 +0100

    Add scripts

---
+++ get_dotfiles.bash
@@ -0,0 +1,8 @@
+mkdir -p Program\ Files\ \(x86\)/NoteBook\ FanControl/Configs
+mkdir -p AppData/Roaming/Sublime\ Text\ 3/Packages/User/
+mkdir -p AppData/Roaming/alacritty/
+cp C:/Program\ Files\ \(x86\)/NoteBook\ FanControl/Configs/HP\ Pavilion\ Gaming\ Laptop\ 15-ec1xxx.xml Program\ Files\ \(x86\)/NoteBook\ FanControl/Configs/
+cp C:/Users/bLeDy/AppData/Roaming/Sublime\ Text\ 3/Packages/User/* AppData/Roaming/Sublime\ Text\ 3/Packages/User/
+cp C:/Users/bLeDy/AppData/Roaming/alacritty/alacritty.yml AppData/Roaming/alacritty
+cp C:/Users/bLeDy/.bashrc .
+cp C:/Users/bLeDy/.gitconfig .
---
+++ set_dotfiles.bash
@@ -0,0 +1,8 @@
+mkdir -p C:/Program\ Files\ \(x86\)/NoteBook\ FanControl/Configs
+mkdir -p C:/Users/bLeDy/AppData/Roaming/Sublime\ Text\ 3/Packages/User/
+mkdir -p C:/Users/bLeDy/AppData/Roaming/alacritty/
+cp Program\ Files\ \(x86\)/NoteBook\ FanControl/Configs/HP\ Pavilion\ Gaming\ Laptop\ 15-ec1xxx.xml C:/Program\ Files\ \(x86\)/NoteBook\ FanControl/Configs/
+cp AppData/Roaming/Sublime\ Text\ 3/Packages/User/* C:/Users/bLeDy/AppData/Roaming/Sublime\ Text\ 3/Packages/User
+cp AppData/Roaming/alacritty/alacritty.yml C:/Users/bLeDy/AppData/Roaming/alacritty
+cp .bashrc C:/Users/bLeDy
+cp .gitconfig C:/Users/bLeDy
