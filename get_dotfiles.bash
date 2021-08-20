83919ee4f3eb710e76c40982d4a8314bdd32d874
Iago Alonso <deejaynof@gmail.com>
2021-08-20 22:57:22 +0200

    fix: Do not use quotes

--- get_dotfiles.bash
+++ get_dotfiles.bash
@@ -1,45 +1,45 @@
 #!/usr/bin/env bash
 
-DOTFILES_HOME="$HOME/workspace/repos/ibLeDy/dotfiles"
-DOTFILES_CONFIG_HOME="$DOTFILES_HOME/.config"
-DOTFILES_LOCAL_BIN="$DOTFILES_HOME/.local/bin"
-DOTFILES_LOCAL_SHARE="$DOTFILES_HOME/.local/share"
+DOTFILES_HOME=$HOME/workspace/repos/ibLeDy/dotfiles
+DOTFILES_CONFIG_HOME=$DOTFILES_HOME/.config
+DOTFILES_LOCAL_BIN=$DOTFILES_HOME/.local/bin
+DOTFILES_LOCAL_SHARE=$DOTFILES_HOME/.local/share
 
 # $HOME
-cp ~/.asoundrc "$DOTFILES_HOME"
-cp ~/.imwheelrc "$DOTFILES_HOME"
-cp ~/.pdbrc "$DOTFILES_HOME"
-cp ~/.profile "$DOTFILES_HOME"
+cp ~/.asoundrc $DOTFILES_HOME
+cp ~/.imwheelrc $DOTFILES_HOME
+cp ~/.pdbrc $DOTFILES_HOME
+cp ~/.profile $DOTFILES_HOME
 
 # $XDG_CONFIG_HOME
-mkdir -p "$DOTFILES_CONFIG_HOME/{alacritty,Code/User,dash-to-panel,flameshot,git,gnome-extensions-sync,htop,i3,i3blocks,lsd,mpv,nano,pulse,sublime-text-3/Packages/User,tmux,tiling-assistant,vim,zellij,zsh}"
-cp ~/.config/aliasrc "$DOTFILES_CONFIG_HOME"
-cp ~/.config/alacritty/alacritty.yml "$DOTFILES_CONFIG_HOME/alacritty/"
-cp ~/.config/Code/User/{keybindings.json,settings.json} "$DOTFILES_CONFIG_HOME/Code/User/"
-cp ~/.config/dash-to-panel/dash-to-panel-settings "$DOTFILES_CONFIG_HOME/dash-to-panel/"
-cp ~/.config/flameshot/flameshot.conf "$DOTFILES_CONFIG_HOME/flameshot/"
-cp ~/.config/git/{config,ignore} "$DOTFILES_CONFIG_HOME/git/"
-cp ~/.config/gnome-extensions-sync/extensions.json "$DOTFILES_CONFIG_HOME/gnome-extensions-sync"
-cp ~/.config/htop/htoprc "$DOTFILES_CONFIG_HOME/htop/"
-cp ~/.config/i3/config "$DOTFILES_CONFIG_HOME/i3/"
-cp ~/.config/i3blocks/config "$DOTFILES_CONFIG_HOME/i3blocks/"
-cp ~/.config/lsd/config.yaml "$DOTFILES_CONFIG_HOME/lsd/"
-cp ~/.config/mpv/mpv.conf "$DOTFILES_CONFIG_HOME/mpv/"
-cp ~/.config/nano/nanorc "$DOTFILES_CONFIG_HOME/nano/"
-cp ~/.config/pulse/{daemon.conf,default.pa} "$DOTFILES_CONFIG_HOME/pulse/"
-cp ~/.config/sublime-text-3/Packages/User/{"Default (Linux).sublime-keymap","Package Control.sublime-settings",Preferences.sublime-settings} "$DOTFILES_CONFIG_HOME/sublime-text-3/Packages/User/"
-cp ~/.config/tmux/tmux.conf "$DOTFILES_CONFIG_HOME/tmux/"
-cp ~/.config/tiling-assistant/layouts.json "$DOTFILES_CONFIG_HOME/tiling-assistant/"
-cp ~/.config/vim/vimrc "$DOTFILES_CONFIG_HOME/vim/"
-cp ~/.config/zellij/config.yaml "$DOTFILES_CONFIG_HOME/zellij/"
-cp ~/.config/zsh/.{p10k.zsh,zshrc} "$DOTFILES_CONFIG_HOME/zsh/"
+mkdir -p $DOTFILES_CONFIG_HOME/{alacritty,Code/User,dash-to-panel,flameshot,git,gnome-extensions-sync,htop,i3,i3blocks,lsd,mpv,nano,pulse,sublime-text-3/Packages/User,tmux,tiling-assistant,vim,zellij,zsh}
+cp ~/.config/aliasrc $DOTFILES_CONFIG_HOME
+cp ~/.config/alacritty/alacritty.yml $DOTFILES_CONFIG_HOME/alacritty/
+cp ~/.config/Code/User/{keybindings.json,settings.json} $DOTFILES_CONFIG_HOME/Code/User/
+cp ~/.config/dash-to-panel/dash-to-panel-settings $DOTFILES_CONFIG_HOME/dash-to-panel/
+cp ~/.config/flameshot/flameshot.conf $DOTFILES_CONFIG_HOME/flameshot/
+cp ~/.config/git/{config,ignore} $DOTFILES_CONFIG_HOME/git/
+cp ~/.config/gnome-extensions-sync/extensions.json $DOTFILES_CONFIG_HOME/gnome-extensions-sync
+cp ~/.config/htop/htoprc $DOTFILES_CONFIG_HOME/htop/
+cp ~/.config/i3/config $DOTFILES_CONFIG_HOME/i3/
+cp ~/.config/i3blocks/config $DOTFILES_CONFIG_HOME/i3blocks/
+cp ~/.config/lsd/config.yaml $DOTFILES_CONFIG_HOME/lsd/
+cp ~/.config/mpv/mpv.conf $DOTFILES_CONFIG_HOME/mpv/
+cp ~/.config/nano/nanorc $DOTFILES_CONFIG_HOME/nano/
+cp ~/.config/pulse/{daemon.conf,default.pa} $DOTFILES_CONFIG_HOME/pulse/
+cp ~/.config/sublime-text-3/Packages/User/{'Default (Linux).sublime-keymap','Package Control.sublime-settings',Preferences.sublime-settings} $DOTFILES_CONFIG_HOME/sublime-text-3/Packages/User/
+cp ~/.config/tmux/tmux.conf $DOTFILES_CONFIG_HOME/tmux/
+cp ~/.config/tiling-assistant/layouts.json $DOTFILES_CONFIG_HOME/tiling-assistant/
+cp ~/.config/vim/vimrc $DOTFILES_CONFIG_HOME/vim/
+cp ~/.config/zellij/config.yaml $DOTFILES_CONFIG_HOME/zellij/
+cp ~/.config/zsh/.{p10k.zsh,zshrc} $DOTFILES_CONFIG_HOME/zsh/
 
 # $HOME/.local/bin
-mkdir -p "$DOTFILES_LOCAL_BIN"
-# cp ~/.local/bin/{countdown,extramaus,fix-volume,mute-spotify-ads} "$DOTFILES_LOCAL_BIN"
-cp ~/.local/bin/fix-screen-tearing "$DOTFILES_LOCAL_BIN"
+mkdir -p $DOTFILES_LOCAL_BIN
+# cp ~/.local/bin/{countdown,extramaus,fix-volume,mute-spotify-ads} $DOTFILES_LOCAL_BIN
+cp ~/.local/bin/fix-screen-tearing $DOTFILES_LOCAL_BIN
 
 # $HOME/.local/share
-mkdir -p "$DOTFILES_LOCAL_SHARE/zellij/{layouts,plugins}"
-cp ~/.local/share/zellij/layouts/{default,strider}.yaml "$DOTFILES_LOCAL_SHARE/zellij/layouts"
-cp ~/.local/share/zellij/plugins/* "$DOTFILES_LOCAL_SHARE/zellij/plugins"
+mkdir -p $DOTFILES_LOCAL_SHARE/zellij/{layouts,plugins}
+cp ~/.local/share/zellij/layouts/{default,strider}.yaml $DOTFILES_LOCAL_SHARE/zellij/layouts
+cp ~/.local/share/zellij/plugins/* $DOTFILES_LOCAL_SHARE/zellij/plugins
--- set_dotfiles.bash
+++ set_dotfiles.bash
@@ -1,45 +1,45 @@
 #!/usr/bin/env bash
 
-DOTFILES_HOME="$HOME/workspace/repos/ibLeDy/dotfiles"
-DOTFILES_CONFIG_HOME="$DOTFILES_HOME/.config"
-DOTFILES_LOCAL_BIN="$DOTFILES_HOME/.local/bin"
-DOTFILES_LOCAL_SHARE="$DOTFILES_HOME/.local/share"
+DOTFILES_HOME=$HOME/workspace/repos/ibLeDy/dotfiles
+DOTFILES_CONFIG_HOME=$DOTFILES_HOME/.config
+DOTFILES_LOCAL_BIN=$DOTFILES_HOME/.local/bin
+DOTFILES_LOCAL_SHARE=$DOTFILES_HOME/.local/share
 
 # $HOME
-cp "$DOTFILES_HOME/.asoundrc" ~/
-cp "$DOTFILES_HOME/.imwheelrc" ~/
-cp "$DOTFILES_HOME/.pdbrc" ~/
-cp "$DOTFILES_HOME/.profile" ~/
+cp $DOTFILES_HOME/.asoundrc ~/
+cp $DOTFILES_HOME/.imwheelrc ~/
+cp $DOTFILES_HOME/.pdbrc ~/
+cp $DOTFILES_HOME/.profile ~/
 
 # $XDG_CONFIG_HOME
 mkdir -p ~/.config/{alacritty,Code/User,dash-to-panel,flameshot,git,gnome-extensions-sync,htop,i3,i3blocks,lsd,mpv,nano,pulse,sublime-text-3/Packages/User,tmux,tiling-assistant,vim,zellij,zsh}
-cp "$DOTFILES_CONFIG_HOME/aliasrc" ~/.config
-cp "$DOTFILES_CONFIG_HOME/alacritty/alacritty.yml" ~/.config/alacritty/
-cp "$DOTFILES_CONFIG_HOME/Code/User/{keybindings.json,settings.json}" ~/.config/Code/User/
-cp "$DOTFILES_CONFIG_HOME/dash-to-panel/dash-to-panel-settings" ~/.config/dash-to-panel/
-cp "$DOTFILES_CONFIG_HOME/flameshot/flameshot.conf" ~/.config/flameshot/
-cp "$DOTFILES_CONFIG_HOME/git/{config,ignore}" ~/.config/git/
-cp "$DOTFILES_CONFIG_HOME/gnome-extensions-sync/extensions.json" ~/.config/gnome-extensions-sync/
-cp "$DOTFILES_CONFIG_HOME/htop/htoprc" ~/.config/htop/
-cp "$DOTFILES_CONFIG_HOME/i3/config" ~/.config/i3/
-cp "$DOTFILES_CONFIG_HOME/i3blocks/config" ~/.config/i3blocks/
-cp "$DOTFILES_CONFIG_HOME/lsd/config.yaml" ~/.config/lsd/
-cp "$DOTFILES_CONFIG_HOME/mpv/mpv.conf" ~/.config/mpv/
-cp "$DOTFILES_CONFIG_HOME/nano/nanorc" ~/.config/nano/
-cp "$DOTFILES_CONFIG_HOME/pulse/{daemon.conf,default.pa}" ~/.config/pulse/
-cp "$DOTFILES_CONFIG_HOME/sublime-text-3/Packages/User/{'Default (Linux).sublime-keymap','Package Control.sublime-settings',Preferences.sublime-settings}" ~/.config/sublime-text-3/Packages/User/
-cp "$DOTFILES_CONFIG_HOME/tmux/tmux.conf" ~/.config/tmux/
-cp "$DOTFILES_CONFIG_HOME/tiling-assistant/layouts.json" ~/.config/tiling-assistant/
-cp "$DOTFILES_CONFIG_HOME/vim/vimrc" ~/.config/vim/
-cp "$DOTFILES_CONFIG_HOME/zellij/config.yaml" ~/.config/zellij/
-cp "$DOTFILES_CONFIG_HOME/zsh/.{p10k.zsh,zshrc}" ~/.config/zsh/
+cp $DOTFILES_CONFIG_HOME/aliasrc ~/.config
+cp $DOTFILES_CONFIG_HOME/alacritty/alacritty.yml ~/.config/alacritty/
+cp $DOTFILES_CONFIG_HOME/Code/User/{keybindings.json,settings.json} ~/.config/Code/User/
+cp $DOTFILES_CONFIG_HOME/dash-to-panel/dash-to-panel-settings ~/.config/dash-to-panel/
+cp $DOTFILES_CONFIG_HOME/flameshot/flameshot.conf ~/.config/flameshot/
+cp $DOTFILES_CONFIG_HOME/git/{config,ignore} ~/.config/git/
+cp $DOTFILES_CONFIG_HOME/gnome-extensions-sync/extensions.json ~/.config/gnome-extensions-sync/
+cp $DOTFILES_CONFIG_HOME/htop/htoprc ~/.config/htop/
+cp $DOTFILES_CONFIG_HOME/i3/config ~/.config/i3/
+cp $DOTFILES_CONFIG_HOME/i3blocks/config ~/.config/i3blocks/
+cp $DOTFILES_CONFIG_HOME/lsd/config.yaml ~/.config/lsd/
+cp $DOTFILES_CONFIG_HOME/mpv/mpv.conf ~/.config/mpv/
+cp $DOTFILES_CONFIG_HOME/nano/nanorc ~/.config/nano/
+cp $DOTFILES_CONFIG_HOME/pulse/{daemon.conf,default.pa} ~/.config/pulse/
+cp $DOTFILES_CONFIG_HOME/sublime-text-3/Packages/User/{'Default (Linux).sublime-keymap','Package Control.sublime-settings',Preferences.sublime-settings} ~/.config/sublime-text-3/Packages/User/
+cp $DOTFILES_CONFIG_HOME/tmux/tmux.conf ~/.config/tmux/
+cp $DOTFILES_CONFIG_HOME/tiling-assistant/layouts.json ~/.config/tiling-assistant/
+cp $DOTFILES_CONFIG_HOME/vim/vimrc ~/.config/vim/
+cp $DOTFILES_CONFIG_HOME/zellij/config.yaml ~/.config/zellij/
+cp $DOTFILES_CONFIG_HOME/zsh/.{p10k.zsh,zshrc} ~/.config/zsh/
 
 # $HOME/.local/bin
 mkdir -p ~/.local/bin
-# cp "$DOTFILES_LOCAL_BIN/{countdown,extramaus,fix-volume,mute-spotify-ads}" ~/.local/bin/
-cp "$DOTFILES_LOCAL_BIN/fix-screen-tearing" ~/.local/bin/fix-screen-tearing
+# cp $DOTFILES_LOCAL_BIN/{countdown,extramaus,fix-volume,mute-spotify-ads} ~/.local/bin/
+cp $DOTFILES_LOCAL_BIN/fix-screen-tearing ~/.local/bin/fix-screen-tearing
 
 # $HOME/.local/share
 mkdir -p ~/.local/share/zellij/{layouts,plugins}
-cp "$DOTFILES_LOCAL_SHARE/zellij/layouts/{default,strider}.yaml" ~/.local/share/zellij/layouts/
-cp "$DOTFILES_LOCAL_SHARE/zellij/plugins/*" ~/.local/share/zellij/plugins/
+cp $DOTFILES_LOCAL_SHARE/zellij/layouts/{default,strider}.yaml ~/.local/share/zellij/layouts/
+cp $DOTFILES_LOCAL_SHARE/zellij/plugins/* ~/.local/share/zellij/plugins/
