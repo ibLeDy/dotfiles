8987fca0b58b2ff15c3e2543c134ca8c672919df
Iago Alonso <deejaynof@gmail.com>
2024-05-30 18:31:13 +0200

    fix: Quote instead of escaping spaces

--- link.sh
+++ link.sh
@@ -25,12 +25,12 @@ link_file "$DOTFILES_HOME"/.profile ~/.profile
 
 # $XDG_CONFIG_HOME
 mkdir -p ~/.config/{alacritty,flameshot,git/{config.d,templates/hooks},htop,lsd,starship,tmux,vim,zellij,zsh}
-mkdir -p ~/Library/Application\ Support/Code/User/
-# mkdir -p ~/Library/Application\ Support/Sublime\ Text\ 3/Packages/User/
+mkdir -p ~/Library/'Application Support'/Code/User
+mkdir -p ~/Library/'Application Support'/'Sublime Text'/Packages/User
 link_file "$DOTFILES_CONFIG_HOME"/aliasrc ~/.config/aliasrc
 link_file "$DOTFILES_CONFIG_HOME"/alacritty/alacritty.yml ~/.config/alacritty/alacritty.yml
-link_file "$DOTFILES_CONFIG_HOME"/Code/User/keybindings.json ~/Library/Application\ Support/Code/User/keybindings.json
-link_file "$DOTFILES_CONFIG_HOME"/Code/User/settings.json ~/Library/Application\ Support/Code/User/settings.json
+link_file "$DOTFILES_CONFIG_HOME"/Code/User/keybindings.json ~/Library/'Application Support'/Code/User/keybindings.json
+link_file "$DOTFILES_CONFIG_HOME"/Code/User/settings.json ~/Library/'Application Support'/Code/User/settings.json
 # link_file "$DOTFILES_CONFIG_HOME"/flameshot/flameshot.conf ~/.config/flameshot/flameshot.conf
 link_file "$DOTFILES_CONFIG_HOME"/git/config ~/.config/git/config
 link_file "$DOTFILES_CONFIG_HOME"/git/ignore ~/.config/git/ignore
@@ -41,11 +41,9 @@ link_file "$DOTFILES_CONFIG_HOME"/git/templates/hooks/pre-commit ~/.config/git/t
 link_file "$DOTFILES_CONFIG_HOME"/lsd/config.yaml ~/.config/lsd/config.yaml
 link_file "$DOTFILES_CONFIG_HOME"/mpv/mpv.conf ~/.config/mpv/mpv.conf
 link_file "$DOTFILES_CONFIG_HOME"/starship/starship.toml ~/.config/starship/starship.toml
-# cp $DOTFILES_CONFIG_HOME/sublime-text/Packages/User/{'Default (OSX).sublime-keymap',Preferences.sublime-settings} ~/Library/Application\ Support/Sublime\ Text\ 3/Packages/User/
-# link_file "$DOTFILES_CONFIG_HOME"/sublime-text/Packages/User/'Default (Linux).sublime-keymap' ~/.config/sublime-text/Packages/User/'Default (Linux).sublime-keymap'
-# link_file "$DOTFILES_CONFIG_HOME"/sublime-text/Packages/User/'Default (Linux).sublime-mousemap' ~/.config/sublime-text/Packages/User/'Default (Linux).sublime-mousemap'
-# link_file "$DOTFILES_CONFIG_HOME"/sublime-text/Packages/User/'Package Control.sublime-settings' ~/.config/sublime-text/Packages/User/'Package Control.sublime-settings'
-# link_file "$DOTFILES_CONFIG_HOME"/sublime-text/Packages/User/Preferences.sublime-settings ~/.config/sublime-text/Packages/User/Preferences.sublime-settings
+link_file "$DOTFILES_CONFIG_HOME"/sublime-text/Packages/User/'Default (OSX).sublime-keymap' ~/Library/'Application Support'/'Sublime Text'/Packages/User/'Default (Linux).sublime-keymap'
+link_file "$DOTFILES_CONFIG_HOME"/sublime-text/Packages/User/'Package Control.sublime-settings' ~/Library/'Application Support'/'Sublime Text'/Packages/User/'Package Control.sublime-settings'
+link_file "$DOTFILES_CONFIG_HOME"/sublime-text/Packages/User/Preferences.sublime-settings ~/Library/'Application Support'/'Sublime Text'/Packages/User/Preferences.sublime-settings
 link_file "$DOTFILES_CONFIG_HOME"/tmux/tmux.conf ~/.config/tmux/tmux.conf
 link_file "$DOTFILES_CONFIG_HOME"/vim/vimrc ~/.config/vim/vimrc
 link_file "$DOTFILES_CONFIG_HOME"/zellij/config.yaml ~/.config/zellij/config.yaml
